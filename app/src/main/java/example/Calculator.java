7a05389755fb0c28bc2c9f7af259fc80e82575bf
Ian Rachinsky <ian.rachinsky@student.unimelb.edu.au>
2025-03-05 14:42:14 +1100

    Added Comments

--- app/src/main/java/example/Calculator.java
+++ app/src/main/java/example/Calculator.java
@@ -1,18 +1,19 @@
 package example;
 
 public class Calculator {
+    // returns sum of two integers
     public int add(int a, int b) {
         return a+b;
     }
-
+    // returns difference of two integers
     public int minus(int a, int b) {
         return a-b;
     }
-
+    // returns product of two integers
     public int multiply(int a, int b) {
         return a*b;
     }
-
+    // returns floating point quotient of two integers
     public double divide(int a, int b) {
         return ((double) a / (double) b);
     }
